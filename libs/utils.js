06e21cfa2537a7404692f02fdddee0e3035dc884
TheUltimateKeval <32948647+TheUltimateKeval@users.noreply.github.com>
2021-02-25 20:55:59 +0530

    Feat: Added more JSDoc cuz am too dumb to work on bot itself

--- libs/utils.js
+++ libs/utils.js
@@ -1,13 +1,26 @@
+const Discord = require("discord.js");
 const { pickups: pCache, pickupsCount: cCache } = require('../app').cache;
 const Pickups = require('../structures/Pickups');
+const Game = require('../structures/Game');
 const db = require('../app').db.channels;
 
+/**
+ * Updates cache with Game data
+ * @param {Game} game 
+ * @param {Pickups} pickups 
+ * @param {Discord.TextChannel} channel 
+ */
 const updateCache = (game, pickups, channel) => {
     if (!(pickups instanceof Pickups)) throw new Error('Received deserialized!');
     pickups.games[game.id] = game;
     pCache[channel.id][pickups.name] = pickups;
 };
 
+/**
+ * Fetches all Pickups for a Channel
+ * @param {Discord.TextChannel} channel
+ * @returns {Array<Pickups>} An Array of Pickups instances
+ */
 const getPickupChannel = async(channel) => {
     let pickupsChannel = pCache[channel];
     if (!pickupsChannel) {
@@ -21,6 +34,7 @@ const getPickupChannel = async(channel) => {
     }
     return pickupsChannel;
 };
+
 module.exports = {
     updateCache,
     getPickupChannel,
